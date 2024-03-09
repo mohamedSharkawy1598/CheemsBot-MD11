ea4bb0ddac2b4695bc1c6429f7ace86b8abba0b8
mohamedSharkawy1598 <124299658+mohamedSharkawy1598@users.noreply.github.com>
2024-03-09 15:36:16 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "𝑺𝑼𝑵𝑮" //ur yt chanel name
+global.socialm = "𝑺𝑼𝑵𝑮" //ur github or insta name
+global.location = "Egypt, Qena, Al-Deir Al-Sharqi" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.botname = '𝑨𝑰𝒁𝑬𝑵 𝑩𝑶𝑻' //ur bot name
+global.ownernumber = '201015411497' //ur owner number
+global.ownername = '𝑺𝑼𝑵𝑮' //ur owner name
+global.websitex = "https://whatsapp.com/channel/0029VaPCJmG7dmeXd15uuf1x"
+global.wagc = "https://whatsapp.com/channel/0029VaPCJmG7dmeXd15uuf1x"
+global.themeemoji = '🧪'
+global.wm = "𝑨𝑰𝒁𝑬𝑵 𝑩𝑶𝑻"
+global.botscript = 'https://whatsapp.com/channel/0029VaPCJmG7dmeXd15uuf1x' //script link
+global.packname = "صلي على النبي "
+global.author = "𝑺𝑼𝑵𝑮\n\n+201015411497"
+global.creator = "201015411497@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["201015411497"] // Premium User
 global.hituet = 0
 
 //bot sett
